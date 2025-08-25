290eeedbeab9e72b82056cc89a9c0539f3527988
Valdemar Emil Otte Petersen <emilotte888@gmail.com>
2025-08-25 10:40:39 +0200

    Update 'read_and_write/sql.py'

--- read_and_write/sql.py
+++ read_and_write/sql.py
@@ -13,11 +13,21 @@ def read_db(path: Path, sql: str):
     Read the database and return the wanted information.
     
     :param Path path: Path to database.
-    :param str sql: The sql statement.
+    :param str sql: The SQL statement.
     :return list: A list of the wanted information from the database.
     """
     with sqlite3.connect(path) as conn:
         cursor = conn.cursor()
         result = cursor.execute(sql)
         return [element for element in result.fetchall()]
-    
+
+
+def executre_sql_on_db(path: Path, sql: str) -> None:
+    """
+    Executre SQL statement on the database.
+
+    :param Path path: Path to database.
+    :param str sql: The SQL statement."""
+    with sqlite3.connect(path) as conn:
+        cursor = conn.cursor()
+        cursor.execute(sql)
