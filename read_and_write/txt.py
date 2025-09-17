ecc16d577ff2b4a40005bb2b45f36175392cdc2a
Valdemar Emil Otte Petersen <emilotte888@gmail.com>
2025-09-17 09:28:06 +0200

    Added encoding for 	xt.py and new function

--- read_and_write/txt.py
+++ read_and_write/txt.py
@@ -7,24 +7,39 @@ from pathlib import Path
 # Local files
 
 
-def read_txt(path: Path) -> list[str]:
+def read_txt(path: Path, encoding: str = "UTF-8") -> list[str]:
     """
     Read txt-file and return lines.
 
     :param Path txt_path: Path to txt-file.
+    :param str encoding: Encoding for the txt-file. Default is 'UTF-8'.
     :return list[str]: A list of strings with the content of the file.
     """
-    with open(path, "r", encoding="UTF-8") as file:
+    with open(path, "r", encoding=encoding) as file:
         return [line.rstrip() for line in file]
 
 
-def write_txt(lines: list[str], path: Path) -> None:
+def write_lines_to_txt(lines: list[str], path: Path, encoding: str = "UTF-8") -> None:
     """
     Write to a txt-file line by line.
 
     :param list[str] lines: List of strings, which will be writed to the txt-file.
+    :param str encoding: Encoding for the txt-file. Default is 'UTF-8'.
     :param Path txt_path: Path to txt-file.
     """
-    with open(path, "w", encoding="UTF-8") as file:
+    with open(path, "w", encoding=encoding) as file:
+        for line in lines:
+            file.write(f"{line}\n")
+
+
+def append_lines_to_txt(lines: list[str], path: Path, encoding: str = "UTF-8") -> None:
+    """
+    Append lines to a txt-file.
+
+    :param list[str] lines: List of strings, which will be appned to the txt-file.
+    :param str encoding: Encoding for the txt-file. Default is 'UTF-8'.
+    :param Path txt_path: Path to txt-file.
+    """
+    with open(path, "w", encoding=encoding) as file:
         for line in lines:
             file.write(f"{line}\n")
